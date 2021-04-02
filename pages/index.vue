ddadc1ec373d506d2e1d111396de977e5cb86e93
Pramit Sawant <pramitsawant11@gmail.com>
2021-04-02 14:04:55 +0530

    project setup

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+
+
+<div class="center">
+ <h1>This is a test site</h1>
+</div>
+
+    
+
+
+
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+
+export default {
+  components: {
+
+  }
+}
+</script>
+
+<style>
+.center {
+  margin: auto;
+  width: 60%;
+  font-size:5em;
+  padding: 10%;
+}
+</style>
